2c1c2f7e837204256dd18ca19e821564e9613b3d
UelWico <gizizula@gmail.com>
2024-05-20 00:21:40 +0700

    Add button save form to NewDog page

--- frontend/src/pages/NewDog.vue
+++ frontend/src/pages/NewDog.vue
@@ -3,19 +3,21 @@ import upperPanel from '@/components/upperPanel.vue'
 import backBtn from '@/components/goBackButton.vue'
 import formForm from '@/components/formForm.vue'
 import cardAndVision from '../components/cardAndVision.vue'
+import addDogButton from '@/components/addDogButton.vue'
 export default {
-  components: { upperPanel, backBtn, formForm, cardAndVision }
+  components: { upperPanel, backBtn, formForm, cardAndVision, addDogButton }
 }
 </script>
 
 <template>
-  <div id="app">
+  <div>
     <upperPanel></upperPanel>
     <backBtn></backBtn>
     <div class="newDBack">
       <cardAndVision></cardAndVision>
       <formForm></formForm>
     </div>
+    <addDogButton></addDogButton>
   </div>
 </template>
 
